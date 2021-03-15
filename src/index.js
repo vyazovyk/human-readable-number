6bebab76a923988c989eb9ef054905370db260ef
Kostya Vyazovyk <vyazovyk@gmail.com>
2021-03-16 00:56:31 +0200

    feat: add human-readable-number solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function toReadable (number) {
-  
+  let massFirst = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+  if (number.lenght = 1) {
+    let humanFirst = number.toString().replace(number, massFirst[number]);
+    return humanFirst;
+  }
 }
